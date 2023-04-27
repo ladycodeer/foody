8fde0549abc68779ee99c08b8e138a812f4b377b
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-27 23:37:24 +0300

    done with the configs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-blue-600">
+    <div className="w-screen h-screen flex flex-col">
       hello everyone
     </div>
   );
