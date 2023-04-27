04b26de04e7dc5df322336c25bd0e18a956b10f4
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-28 00:06:04 +0300

    ı do not know what is the promlem with me today need to go

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Header } from "./components"
 
 
 const App = () => {
   return (
-    <div className="w-screen h-screen flex flex-col">
-      hello everyone
+    <div className="w-screen h-auto flex flex-col ">
+      <Header />
     </div>
   );
 }
