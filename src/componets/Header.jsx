66a12bb428bc077a919d60375da379f7534b2f29
Rabia Kahya <rabiakahya22@gmail.com>
2023-04-29 18:44:52 +0300

    new changes

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Logo from "../img/logo.png"
 
-function Header() {
+const Header = () => {
     return (
-        <div>Header</div>
+        <Header className='fixed z-50 w-screen bg-slate-300  p-6 px-16'>
+            {/* Desktop & Tablet */}
+            <div className='hidden md:flex w-full h-full '>
+                <div className='flex items-center gap-2'>
+                    <img src={Logo} className=' w-8 object-cover ' alt="logo" />
+                    <p className='text-headingColor text-xl font-bold'>Foody</p>
+                </div>
+                <ul className='flex items-center gap-8 ml-auto '>
+                    <li>Home</li>
+                    <li>Menu</li>
+                    <li>About Us</li>
+                    <li>Service</li>
+
+                </ul>
+
+            </div>
+
+
+
+            {/* mobile */}
+            <div className='flex md:hidden h-full '></div>
+
+
+        </Header>
     )
 }
 
-export default Header
+export default Header;
